dd08d3435b29aa0bcf1adc423a7b76db8d32cedc
dojhoanrd-jw <dojhoanrd.jw@gmail.com>
2025-03-03 16:47:50 -0400

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index', { titulo: 'FinApp' });
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
